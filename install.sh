33d870b339cf630989bb7e86f22634041d3f60e9
Alexandre GARINO <alexandre.garino@odaceo.ch>
2017-01-28 00:11:44 +0100

    Apply the new copyright banner

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright Odaceo 2016
+# Copyright (C) 2016 Odaceo. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
